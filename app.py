f26e37d674a352ab87c04c0811b1f6ac818ef768
Ranwiesiel <87268415+Ranwiesiel@users.noreply.github.com>
2024-06-25 11:09:14 +0700

    Penyesuaian code untuk keperluan sendiri

--- app.py
+++ app.py
@@ -1,41 +1,82 @@
 from flask import Flask, render_template, request, jsonify
-from transformers import BertTokenizer, BertModel
-from rank_bm25 import BM25Okapi
 import pandas as pd
-import torch
 import pickle
 import json
-with open('bert_embeddings.pkl', 'rb') as f:
-    df = pickle.load(f)
-with open('bm25_model.pkl', 'rb') as f:
-    bm25 = pickle.load(f)
-with open('data/json/all_meta.json') as f:
-    meta_data = [json.loads(line) for line in f]
-meta_df = pd.DataFrame(meta_data)
-df = pd.merge(df, meta_df, on='id')
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 @app.route('/query', methods=['POST'])
+
+
 def query():
+    with open('bert_embeddings.pkl', 'rb') as f:
+        df = pickle.load(f)
+    with open('bm25_model.pkl', 'rb') as f:
+        bm25 = pickle.load(f)
+    with open('data/json/all_meta.json') as f:
+        meta_data = [json.loads(line) for line in f]
+
+    bm25_model = bm25['bm25']
+    bm25Id = bm25['id']
+
+    default_verdicts = ["guilty", "lepas", "bebas"]
+    results = []
+    
     try:
-        user_query = request.form['query']
+        meta_df = pd.DataFrame(meta_data)
+        user_query = request.form.get('query').lower()
+        get_filter = request.form.get('verdict')
+        # print(get_filter)
+
+        if get_filter == '':
+            meta_df_filtered = meta_df[meta_df['verdict'].isin(default_verdicts)]
+        else:
+            meta_df_filtered = meta_df[meta_df['verdict'] == get_filter]
+
         tokenized_query = user_query.split()
-        scores = bm25.get_scores(tokenized_query)
-        best_idx = scores.argmax()
-        best_doc = df.iloc[best_idx]
-        result = {
-            'id': best_doc['id'],
-            'text': best_doc['text'],
-            'verdict': best_doc['verdict'],
-            'indictment': best_doc['indictment'],
-            'lawyer': best_doc['lawyer'],
-            'owner': best_doc['owner']
-        }
-        return render_template('results.html', result=result)
+        scores = bm25_model.get_scores(tokenized_query)
+        scores = list(zip(bm25Id, scores))
+        filtered_scores = [(id, score) for id, score in scores if id in meta_df_filtered['id'].values]
+
+        filtered_scores.sort(key=lambda x: x[1], reverse=True)
+
+        if not filtered_scores:
+            print("No matching documents found after filtering.")
+        else:
+            # best_id = filtered_scores[0][0]
+            for idx, (best_id, score) in enumerate(filtered_scores):
+                df_filtered = df[df['id'] == best_id]
+                meta_df_best = meta_df_filtered[meta_df_filtered['id'] == best_id]
+
+                best_doc = pd.merge(df_filtered, meta_df_best, on='id')
+        # best_idx = scores.argmax()
+        # df = pd.merge(df, meta_df, on='id')
+        # best_doc = df.iloc[best_idx]
+
+                result = {
+                    'id': best_doc['id'].values[0],
+                    'text': best_doc['text'].values[0],
+                    'verdict': best_doc['verdict'].values[0],
+                    'indictment': best_doc['indictment'].values[0],
+                    'lawyer': best_doc['lawyer'].values[0],
+                    'owner': best_doc['owner'].values[0],
+                    'score': score
+                }
+
+                if score <= 0:
+                    return render_template('results.html', results=results)
+
+                results.append(result)
+                print(f"Result {idx + 1}")
+    
     except Exception as e:
         print(f"Error: {e}")
         return render_template('index.html', error="Error occurred while fetching results.")
+    
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5005)
+    app.run(debug=True)
--- modelling.py
+++ modelling.py
@@ -6,6 +6,8 @@ import pandas as pd
 from tqdm.notebook import tqdm
 from rank_bm25 import BM25Okapi
 from transformers import BertTokenizer, BertModel
+
+
 data = open('data/json/all.json').read()
 list_data = data.split('\n')
 dicts = []
@@ -21,7 +23,9 @@ for line in list_data:
                 'text-tags': text_tags,
                 'text': text
             })
+
 df = pd.DataFrame(dicts)
+
 def extract_entities(text, tags):
     entities = []
     current_entity = []
@@ -40,9 +44,12 @@ def extract_entities(text, tags):
     if current_entity:
         entities.append(' '.join(current_entity))
     return entities
+
 df['entities'] = df.apply(lambda row: extract_entities(row['text'], row['text-tags']), axis=1)
-tokenizer = BertTokenizer.from_pretrained('indobert-base-p1')
-model = BertModel.from_pretrained('indobert-base-p1')
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-indonesian')
+model = BertModel.from_pretrained('bert-base-indonesian')
+
 def bert_encode(texts, batch_size=32, max_length=512):
     embeddings = []
     for i in tqdm(range(0, len(texts), batch_size)):
@@ -52,10 +59,18 @@ def bert_encode(texts, batch_size=32, max_length=512):
             outputs = model(**encoded)
         embeddings.extend(outputs.last_hidden_state.mean(dim=1).cpu().numpy())
     return embeddings
+
 df['text'] = df['text'].apply(lambda x: ' '.join(x) if isinstance(x, list) else x)  
 df['bert_embedding'] = bert_encode(df['text'].tolist(), batch_size=32, max_length=128)
 df.to_pickle('bert_embeddings.pkl')
+
 df['bm25_tokens'] = df['text'].apply(lambda x: x.split())
-bm25 = BM25Okapi(df['bm25_tokens'].tolist())
+bm25_tokens = BM25Okapi(df['bm25_tokens'].tolist())
+
+bm25 = {
+    'bm25': bm25_tokens,
+    'id': df['id'].tolist()
+}
+
 with open('bm25_model.pkl', 'wb') as f:
     pickle.dump(bm25, f)
